56cd4b2bff4e001a4d5cbcf635686ba0662195fb
kalledk21 <kaspermho@gmail.com>
2022-06-09 13:15:18 +0200

    Better editor

--- Assets/Editors/NestWindow.cs
+++ Assets/Editors/NestWindow.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class NestWindow : EditorWindow
@@ -13,12 +14,33 @@ public class NestWindow : EditorWindow
     public static void ShowWindow()
     {
         //Show existing window instance. If one doesn't exist, make one.
-        EditorWindow.GetWindow(typeof(NestWindow)).name = nameof(NestWindow);
+        var window = (NestWindow)EditorWindow.GetWindow(typeof(NestWindow));
+        window.Init();
+    }
+
+    private void OnDestroy()
+    {
+        EditorSceneManager.sceneOpened -= TryFindDataShowcase;
+    }
+
+    public void Init()
+    {
+        name = nameof(NestWindow);
+        TryFindDataShowcase(new UnityEngine.SceneManagement.Scene(), OpenSceneMode.Single);
+        EditorSceneManager.sceneOpened += TryFindDataShowcase;
+    }
+
+    public void TryFindDataShowcase(UnityEngine.SceneManagement.Scene scene, OpenSceneMode mode)
+    {
+        var obj = FindObjectOfType<ShaderArrayFiller>();
+        if (obj != null)
+        {
+            Filler = obj;
+        }
     }
 
     void OnGUI()
     {
-
         var fieldVal = EditorGUILayout.ObjectField(Filler, typeof(UnityEngine.Object), true) as GameObject;
         if (fieldVal != null)
             Filler = fieldVal.GetComponent<ShaderArrayFiller>();
